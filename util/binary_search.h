44b340898bd6cdf562f79287913dfada067ccd26
nozomi203 <takanozomi0505@gmail.com>
2025-01-18 21:58:51 +0900

    二分探索

---
+++ util/binary_search.h
@@ -0,0 +1,17 @@
+#pragma once
+#include <cmath>
+#include <concepts>
+#include <functional>
+
+namespace util {
+
+template <std::integral Int>
+inline std::pair<Int, Int> binary_search(Int ok, Int ng,
+                                         std::function<bool(Int value)> check) {
+  while (std::abs(ok - ng) > 1) {
+    auto mid = (ok + ng) / 2;
+    check(mid) ? ok = mid : ng = mid;
+  }
+  return {ok, ng};
+}
+}  // namespace util
