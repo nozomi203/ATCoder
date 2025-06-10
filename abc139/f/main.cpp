a99971e263d78b87d51c567aa656c6f0c793bf15
nozomi203 <63065128+nozomi203@users.noreply.github.com>
2025-06-10 23:02:50 +0900

    solving abc139-f

--- abc139/f/main.cpp
+++ abc139/f/main.cpp
@@ -1,4 +1,9 @@
 #include "util/common.h"
 
+using vec2 = pair<int64_t, int64_t>;
 int main() {
+  size_t n;
+  cin >> n;
+  vector<vec2> xys(n);
+  for (size_t i{0}; i < n; ++i) cin_pair(xys[i]);
 }
