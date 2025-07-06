89a04a8e15cc66e17952c4ee04d14904698a1622
nozomi203 <63065128+nozomi203@users.noreply.github.com>
2025-07-06 21:14:40 +0900

    solve joi2025ho-a

--- joi2025ho/a/main.cpp
+++ joi2025ho/a/main.cpp
@@ -1,4 +1,41 @@
 #include "util/common.h"
 
 int main() {
+  s64 n;
+  cin >> n;
+  vector<s64> as(n), bs(n);
+  util::cin(as);
+  util::cin(bs);
+
+  map<s64, s64> cnts;
+  for (s64 i = 0; i < n; ++i) ++cnts[as[i]];
+  for (s64 i = 1; i < n; ++i) ++cnts[bs[i]];
+  for (s64 i = 2; i < n; ++i) {
+    as[i] = max(as[i], as[i - 1]);
+    bs[i] = max(bs[i], bs[i - 1]);
+  }
+
+  {
+    s64 j = 1;
+    for (s64 i = 1; i < n; ++i) {
+      while (j < n && as[i] >= bs[j]) ++j;
+      cnts[as[i]] += j - 1;
+    }
+  }
+  {
+    s64 j = 1;
+    for (s64 i = 1; i < n; ++i) {
+      while (j < n && bs[i] > as[j]) ++j;
+      cnts[bs[i]] += j - 1;
+    }
+  }
+  s64 nmax = 0;
+  s64 vmax = 0;
+  for (auto [v, n] : cnts) {
+    if (n >= nmax) {
+      nmax = n;
+      vmax = v;
+    }
+  }
+  cout << vmax << " " << nmax << endl;
 }
