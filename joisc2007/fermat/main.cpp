90a378eea33da114d0eb4696808318b7974af66b
nozomi203 <63065128+nozomi203@users.noreply.github.com>
2025-07-06 23:51:55 +0900

    solving joisc2007-fernat

--- joisc2007/fermat/main.cpp
+++ joisc2007/fermat/main.cpp
@@ -1,3 +1,13 @@
-#include "util/common.h"
+#include <atcoder/math>
 
-int main() {}
+#include "util/common.h"
+int main() {
+  u64 p;
+  cin >> p;
+  u64 n;
+  cin >> n;
+
+  vector<u64> vs(p);
+  for (u64 i = 0; i < p; ++i) vs[i] = atcoder::pow_mod(i, n, p);
+  util::sort(vs);
+}
