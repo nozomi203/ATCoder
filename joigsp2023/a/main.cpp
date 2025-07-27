2f8586849ecbb63c054334e487d39979061bb3d7
nozomi203 <takanozomi0505@gmail.com>
2025-07-28 08:02:32 +0900

    solve joigsp2023 a

--- joigsp2023/a/main.cpp
+++ joigsp2023/a/main.cpp
@@ -7,4 +7,34 @@ int main() {
   util::cin(xhs);
   vector<s64> ts(q);
   util::cin(ts);
+  sort(xhs.begin(), xhs.end());
+  {
+    vector<pair<s64, s64>> xhstmp;
+    xhstmp.reserve(n);
+    for (size_t i = 0; i < n; ++i) {
+      const auto [x, h] = xhs[i];
+      if (xhstmp.empty() || xhstmp.back().second <= h)
+        xhstmp.push_back(make_pair(x, h));
+    }
+    swap(xhstmp, xhs);
+  }
+
+  vector<s64> thresholds(xhs.size());
+  thresholds[0] = xhs[0].first;
+  for (s64 i = 1; i < xhs.size(); ++i) {
+    const auto [xp, hp] = xhs[i - 1];
+    const auto [xc, hc] = xhs[i];
+    thresholds[i] = xc + hp;
+  }
+
+  for (auto t : ts) {
+    auto it = upper_bound(thresholds.begin(), thresholds.end(), t);
+    if (it == thresholds.begin()) {
+      cout << 0 << endl;
+    } else {
+      const auto idx = distance(thresholds.begin(), it - 1);
+      const auto [x, h] = xhs[idx];
+      cout << min(h, t - x) << endl;
+    }
+  }
 }
