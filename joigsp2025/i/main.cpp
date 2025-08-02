ed23eebf3f825746289e8621385c08baae3f4e97
nozomi203 <63065128+nozomi203@users.noreply.github.com>
2025-08-02 12:11:26 +0900

    solve joigsp2025 i

--- joigsp2025/i/main.cpp
+++ joigsp2025/i/main.cpp
@@ -1,6 +1,11 @@
 #include "util/common.h"
 
 /*
+幸福度の高い紅茶から順に同じブランドのケーキのうち幸福度の高いものから順にあてがう。
+同じブランドのケーキが無かった場合は一旦架空のブランドの幸福度0のケーキをあてがう
+あてがい終わったら、残ったケーキを幸福度の高い順に置き換えようとする。
+あてがう先は、幸福度の小さい方から確認する
+置き換えができなくなったら、それ以上先を見ても意味がない。交換の可能性があるのは、ブランドが違うが幸福度めちゃ高なケーキ
  */
 
 int main() {
@@ -15,21 +20,41 @@ int main() {
   sort(bas.begin(), bas.end(), greater());
   sort(dcs.begin(), dcs.end(), greater());
 
-  vector<queue<s64>> mp(n + 1);
-  for (auto [b, a] : bas) mp[a].push(b);
+  vector<queue<s64>> bss(n + 1);
+  for (auto [b, a] : bas) bss[a].push(b);
 
-  const auto get_score = [](const tuple<s64, s64, s64, s64>& t) -> {
-    const auto& [d, c, b, a] = t;
-    return b + (c == a ? d : 0);
-  };
-
-  vector<tuple<s64, s64, s64, s64>> sels;
+  priority_queue<s64, vector<s64>, greater<s64>> sels;
   for (auto [d, c] : dcs) {
-    if (mp[c].empty()) {
-      sels.push_back(make_tuple(d, c, 0, -1));
-    } else {
-      sels.push_back(make_tuple(d, c, mp[c].front(), c));
-      mp[c].pop();
+    if (!bss[c].empty()) {
+      sels.push(d + bss[c].front());
+      bss[c].pop();
     }
   }
+
+  priority_queue<s64> rembs;
+  for (auto& bs : bss) {
+    while (!bs.empty()) {
+      rembs.push(bs.front());
+      bs.pop();
+    }
+  }
+
+  s64 ans = 0;
+  for (s64 i = 0; i < m - sels.size(); ++i) {
+    ans += rembs.top();
+    rembs.pop();
+  }
+
+  while (!sels.empty()) {
+    if (!rembs.empty() && sels.top() < rembs.top()) {
+      ans += rembs.top();
+      rembs.pop();
+      sels.pop();
+    } else {
+      ans += sels.top();
+      sels.pop();
+    }
+  }
+
+  cout << ans << endl;
 }
