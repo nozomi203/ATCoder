ae31e4a33182c1759134c93e29217b02a08b79ee
nozomi203 <63065128+nozomi203@users.noreply.github.com>
2025-08-11 13:39:17 +0900

    solve joi2009ho d,e

--- joi2009ho/d/main.cpp
+++ joi2009ho/d/main.cpp
@@ -1,3 +1,50 @@
 #include "util/common.h"
 
-int main() {}
+/*
+dp[i][j] = 太郎君が交差点(i,j)をn-1回目までのさんぽで訪れる回数
+dp[0][0] = n-1
+(i-1,j)の初期値が0
+奇数回のとき(i,j)を訪れる
+(i-1,j)の初期値が1
+偶数回のとき(i,j)を訪れる
+dp[i][j] +=
+*/
+
+int main() {
+  s64 h, w, n;
+  cin >> h >> w >> n;
+  vector<vector<s64>> as(h, vector<s64>(w));
+  util::cin(as);
+
+  vector<vector<s64>> dp(h, vector<s64>(w));
+  dp[0][0] = n - 1;
+  for (s64 i = 0; i < h; ++i) {
+    for (s64 j = 0; j < w; ++j) {
+      if (i > 0) {
+        if (as[i - 1][j] == 0) {
+          dp[i][j] += (dp[i - 1][j] + 1) / 2;
+        } else {
+          dp[i][j] += dp[i - 1][j] / 2;
+        }
+      }
+      if (j > 0) {
+        if (as[i][j - 1] == 0) {
+          dp[i][j] += dp[i][j - 1] / 2;
+        } else {
+          dp[i][j] += (dp[i][j - 1] + 1) / 2;
+        }
+      }
+    }
+  }
+  s64 i = 0, j = 0;
+  while (i < h && j < w) {
+    const s64 a = (as[i][j] + dp[i][j]) % 2;
+    if (a == 0) {
+      ++i;
+    } else {
+      ++j;
+    }
+  }
+
+  cout << i + 1 << " " << j + 1 << endl;
+}
--- joi2009ho/e/main.cpp
+++ joi2009ho/e/main.cpp
@@ -1,3 +1,108 @@
 #include "util/common.h"
 
-int main() {}
+/*
+認証レベルの和lで二分探索
+lが与えられたとき、部屋をr以上訪れることができるかが高速に分かればいい
+
+
+*/
+
+int main() {
+  s64 r;
+  cin >> r;
+  s64 w0, h0, y0, x0;
+  cin >> w0 >> h0 >> y0 >> x0;
+  vector<vector<s64>> l0ss(h0, vector<s64>(w0));
+  util::cin(l0ss);
+  s64 w1, h1, y1, x1;
+  cin >> w1 >> h1 >> y1 >> x1;
+  vector<vector<s64>> l1ss(h1, vector<s64>(w1));
+  util::cin(l1ss);
+
+  --x0;
+  --y0;
+  --x1;
+  --y1;
+
+  const auto calc = [&](const vector<vector<s64>>& lss, s64 sx,
+                        s64 sy) -> vector<pair<s64, s64>> {
+    vector<pair<s64, s64>> ret;
+    ret.push_back(make_pair(0, 0));
+    const s64 h = lss.size();
+    const s64 w = lss[0].size();
+    vector<vector<bool>> visited(h, vector<bool>(w));
+    using elem_t = tuple<s64, s64, s64>;
+    priority_queue<elem_t, vector<elem_t>, greater<elem_t>> pq;
+    pq.push(make_tuple(1, sx, sy));
+
+    const auto expand = [&](s64 x, s64 y) {
+      visited[x][y] = true;
+      const pair<s64, s64> dxys[] = {
+          make_pair(0, 1),
+          make_pair(0, -1),
+          make_pair(1, 0),
+          make_pair(-1, 0),
+      };
+      for (auto [dx, dy] : dxys) {
+        auto nx = x + dx;
+        auto ny = y + dy;
+        if (nx < 0 || nx >= h || ny < 0 || ny >= w) continue;
+        if (visited[nx][ny]) continue;
+        pq.push(make_tuple(lss[nx][ny], nx, ny));
+      }
+    };
+
+    while (!pq.empty()) {
+      auto [l, x, y] = pq.top();
+      pq.pop();
+      if (visited[x][y]) continue;
+      if (ret.back().first < l) {
+        ret.push_back(make_pair(l, ret.back().second));
+      }
+
+      ++ret.back().second;
+      expand(x, y);
+    }
+
+    return ret;
+  };
+
+  vector<pair<s64, s64>> lc0s(calc(l0ss, x0, y0)), lc1s(calc(l1ss, x1, y1));
+
+  const auto check = [&](s64 l) -> bool {
+    for (auto [l0, c0] : lc0s) {
+      if (l0 > l) break;
+      if (c0 > r) return true;
+
+      auto lrem = l - l0;
+      auto crem = r - c0;
+
+      auto it = lower_bound(
+          lc1s.begin(), lc1s.end(), crem,
+          [](const pair<s64, s64>& e, s64 val) { return e.second < val; });
+      if (it == lc1s.end()) {
+        continue;
+      } else {
+        auto [l1, c1] = *it;
+        if (l1 <= lrem) return true;
+      }
+    }
+
+    return false;
+  };
+
+  const auto get = [&]() -> s64 {
+    s64 ll = 0, lr = 200000000;
+    while (lr - ll > 1) {
+      s64 l = (ll + lr) / 2;
+      if (check(l)) {
+        lr = l;
+      } else {
+        ll = l;
+      }
+    }
+    return lr;
+  };
+
+  cout << get() << endl;
+}
